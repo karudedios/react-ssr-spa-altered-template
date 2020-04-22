aca8de69b8397e926ac4af29924a2d62e70cfc44
Carlos de Dios <carlos.ferreras@avantica.net>
2020-04-21 19:15:28 -0600

    Refactoring AboutPage into using hooks
    - Created hook for client-side data loads when needed
    - Bumping React to 16.8 for Hook support

--- src/app/about_page/about_page.js
+++ src/app/about_page/about_page.js
@@ -1,67 +1,42 @@
 import get from 'lodash/get';
 import PropTypes from 'prop-types';
-import React, { Component, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import withRouter from 'react-router/withRouter';
 import Link from 'react-router-dom/Link';
 
-import loadData from './about_page_data_fetch';
-
+import fetchAboutPageData from './about_page_data_fetch';
 import Footer from '../components/footer/footer';
+import { useClientLoadData } from '../hooks/useClientLoadData';
 
-function AboutPageComponent(props) {
-  useEffect(() => {
-    if (!get(props, 'state.config.initialPageLoad')) {
-      loadData(props.match, props.dispatch, props.state);
-    }
-  }, []);
+function AboutPage(props) {
+  useClientLoadData(props, fetchAboutPageData);
 
   return (
     <div className="about-page">
       <h1>What's this about?</h1>
+
       <p>
         This project aims to do one thing well: make server side rendering
         simple in a react application using only mature community maintained
         libraries.
         <Link to="/repo/michaelBenin/react-ssr-spa">demo: react-ssr-spa</Link>
       </p>
+
       <Footer />
     </div>
   );
 }
 
-class AboutPage extends Component {
-  componentWillMount() {
-    if (!get(this.props, 'state.config.initialPageLoad')) {
-      loadData(this.props.match, this.props.dispatch, this.props.state);
-    } else {
-      // TODO: warm cache for PWA, don't trigger render
-    }
-  }
-
-  render() {
-    return (
-      <div className="about-page">
-        <h1>What's this about?</h1>
-        <p>
-          This project aims to do one thing well: make server side rendering
-          simple in a react application using only mature community maintained
-          libraries.
-          <Link to="/repo/michaelBenin/react-ssr-spa">demo: react-ssr-spa</Link>
-        </p>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-AboutPageComponent.propTypes = {
+// You'll have these 3 props always, what changes is the values they havae depending
+// on whether or not you're rendering from the server or the client
+AboutPage.propTypes = {
   match: PropTypes.shape({}).isRequired,
   dispatch: PropTypes.func.isRequired,
   state: PropTypes.shape({}).isRequired
 };
 
-AboutPageComponent.defaultProps = {};
+AboutPage.defaultProps = {};
 
 function mapStateToProps(state = {}) {
   return {
@@ -69,4 +44,4 @@ function mapStateToProps(state = {}) {
   };
 }
 
-export default withRouter(connect(mapStateToProps)(AboutPageComponent));
+export default withRouter(connect(mapStateToProps)(AboutPage));
---
+++ src/app/hooks/useClientLoadData.js
@@ -0,0 +1,68 @@
+import get from 'lodash/get';
+import { useEffect } from 'react';
+
+/**
+ * Hook that ensures for a given component we determine if a data load is
+ * necessary depending on whether or not the component was rendered from client or server
+ *
+ * @param {Object} props Props containing { match, dispatch, state }
+ * @param {Function} loadDataFn Function to invoke the action from
+ *
+ * @example
+ * // user_list_action_creators.js
+ * export const userListLoading = () => ({
+ *  type: 'USER_LIST_LOADING',
+ *  payload: { isLoading: true }
+ * });
+ *
+ * export const userListLoaded = (users = []) => ({
+ *  type: 'USERS_LOADED',
+ *  payload: { isLoading: false, users }
+ * });
+ *
+ * export const userListLoaded = (err) => ({
+ *  type: 'USERS_ERROR',
+ *  payload: { isLoading: false, err }
+ * });
+ *
+ * // user_list_actions.js
+ * import { userListLoading, userListLoaded, userListError } from './user_action_creators';
+ *
+ * export const fetchUserList = async (queryParams, dispatch, state) => {
+ *  dispatch(userListLoading());
+ *
+ *  try {
+ *    const response = await fetch('https://jsonplaceholder.com/api/users');
+ *    const users = await response.json();
+ *
+ *    dispatch(userListLoaded(users));
+ *  }
+ *  catch(e) {
+ *    dispatch(userListLoaded(e));
+ *  }
+ * }
+ *
+ * // user_list.js
+ * import { fetchUserList } from './user_action_creators';
+ *
+ * function UserList(props) {
+ *  // this will only be executed when we render UserList from the client
+ *  // if we enter directly from the server this is skipped, given that the same data
+ *  // is fetched in the Server and returned with the markup so we don't have to do it again.
+ *  useLoadData(props, fetchUserList);
+ *
+ *  // do things based on props.isLoading, propUsers and prop.error
+ * }
+ *
+ * const mapStateToProps = ({ user: { isLoading, users, error }}) => ({ isLoading, users, error });
+ *
+ * export default withRouter(connect(mapStateToProps)(UserList));
+ */
+export const useClientLoadData = (props, loadDataFn) => {
+  useEffect(() => {
+    // This flag is `true` whenever you're rendering from the server
+    if (!get(props, 'state.config.initialPageLoad')) {
+      loadDataFn(props.match, props.dispatch, props.state);
+    }
+  }, []);
+};
